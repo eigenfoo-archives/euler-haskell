65c36e574df8c93dd7c2881cbd83f7f263750ee7
George Ho <georgh0021@gmail.com>
2017-08-10 07:53:29 +0800

    BLD euler 5

---
+++ euler005.hs
@@ -0,0 +1,22 @@
+import Data.List
+
+main = print $ product $ foldl1 lcm_primes $ map prime_factors [1..20]
+
+lcm_primes :: (Eq a) => [a] -> [a] -> [a]
+lcm_primes a b = a ++ (b\\a)
+
+prime_factors :: (Integral a) => a -> [a]
+prime_factors 1 = []
+prime_factors n
+    | factor == [] = [n]
+    | n < (head factor)^2 = [n]   -- stop searching if sqrt n < head factor
+    | otherwise = factor ++ prime_factors (n `div` (head factor))
+        where factor = take 1 [x | x <- [2..n-1], n `rem` x == 0]
+
+{-
+Naive solution: (takes too long and does not scale)
+  main = print $ head $ filter evenlyDivisible1_20 [1..]
+
+  evenlyDivisible1_20 :: (Integral a) => a -> Bool
+  evenlyDivisible1_20 n = and $ map (== 0) $ map (n `rem`) [1..20]
+-}
