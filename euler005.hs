b08b73995e2dffed7af53e64105a2dc80303c9da
George Ho <georgh0021@gmail.com>
2017-08-10 19:14:26 +0800

    STY proper indentation

--- euler005.hs
+++ euler005.hs
@@ -8,9 +8,9 @@ lcm_primes a b = a ++ (b \\ a)
 prime_factors :: (Integral a) => a -> [a]
 prime_factors 1 = []
 prime_factors n
-  | factor == [] = [n]
+  | factor == []        = [n]
   | n < (head factor)^2 = [n]   -- stop searching if sqrt n < head factor
-  | otherwise = factor ++ prime_factors (n `div` (head factor))
+  | otherwise           = factor ++ prime_factors (n `div` (head factor))
     where factor = take 1 [x | x <- [2..n-1], n `rem` x == 0]
 
 {-
--- euler007.hs
+++ euler007.hs
@@ -4,7 +4,7 @@ main = print $ primes !! 10000 -- zero indexing; 10001st prime
 prime_factors :: (Integral a) => a -> [a]
 prime_factors 1 = []
 prime_factors n
-  | factor == [] = [n]
+  | factor == []        = [n]
   | n < (head factor)^2 = [n]   -- stop searching if sqrt n < head factor
-  | otherwise = factor ++ prime_factors (n `div` (head factor))
+  | otherwise           = factor ++ prime_factors (n `div` (head factor))
     where factor = take 1 [x | x <- [2..n-1], n `rem` x == 0]
